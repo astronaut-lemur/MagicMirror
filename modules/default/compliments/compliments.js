692199ff330163ee53a16f3ff698234655f81197
astronaut-lemur <41335987+astronaut-lemur@users.noreply.github.com>
2018-07-17 23:37:17 +0100

    Update compliments.js

--- modules/default/compliments/compliments.js
+++ modules/default/compliments/compliments.js
@@ -31,7 +31,11 @@ Module.register("compliments", {
 				"Four or five moments, that's all it takes.",
 				"Nobody ever died from love.",
 				"That's slander.",
-				"I'm a big fan."
+				"I'm a big fan.",
+				"You're more fun than bubble wrap!",
+				"<3",
+				"'Stress, insomnia and regret.'",
+				"No, your Nan's a cannibal!"
 				
 			],
 			morning: [
@@ -39,40 +43,86 @@ Module.register("compliments", {
 				"Have a great day!",
 				"Enjoy your day!",
 				"Be you today.",
-				"How was your sleep?"
+				"How was your sleep?",
+				"Today is going to be a good day, and here's why...",
+				"Hope you had a good night's sleep!",
+				":)",
+				"<3",
+				"Eat breakfast!",
+				"Breakfast is the most important meal of the day.",
+				"Suit up!",
+				"Avengers eat breakfast.",
+				"10/10",
+				"Fiiiiiiiiine",
+				"Dang",
+				"Today will be great.",
+				"I've got a good feeling about today."
+			
 			],
 			afternoon: [
 				"Hey dude!",
 				"Today is going to be a good day!",
 				"Have a good day!",
-				"Have you had lunch yet?"
+				"Have you had lunch yet?",
+				"Make sure you eat!",
+				"Isn't it always afternoon?",
+				"<3"
 			],
 			evening: [
 				"Damn, son!",
 				"You hungry?",
 				"How was your day?",
-				"Rest is for the weary, sleep is for the dead."
+				"Rest is for the weary, sleep is for the dead.",
+				"Go to bed already!",
+				"Sleep is for the weak.",
+				"You'll regret staying up late at 7AM tommorrow...",
+				"<3",
+				"It's past your bedtime.",
+				"I hope today was a good one.",
+				"Everything takes time.",
+				"Homework?",
+				"Letters from school?",
+				"Was today good?",
+				"Have a good night!"
 			],
 			day_sunny: [
 				"Remember sunglasses!",
 				"Agh! It burns!",
-				"Don't forget suncream!"
+				"Don't forget suncream!",
+				"<3"
 			],
 			day_cloudy: [
 				"The weather is Scottish.",
-				"It's... grey."
+				"It's... grey.",
+				"It's average outside.",
+				"<3"
 			],
 			snow: [
 				"Look. Outside.",
 				"Amazing weather alert!",
 				"Dude, it's snowing!",
-				"Get your snowboard!"
+				"Get your snowboard!",
+				"It's beginning to look a lot like Christmas!",
+				"Today is going to be amazing.",
+				"<3"
 			],
 			rain: [
 				"It's raining.",
 				"Might need an umbrella.",
-				"Bring a jacket today."
+				"Bring a jacket today.",
+				"<3"
 			],
+			showers: [
+				"A jacket might be a good idea.",
+				"Might need an umbrella today."
+			],
+			fog: [
+				"Good luck with seeing anything today!"
+			],
+			cloudy_windy: [
+				"Might need to equip iron boots!"
+			],
+			
 			
 		},
 		updateInterval: 30000,
